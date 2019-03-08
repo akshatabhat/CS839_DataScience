9cec241fe20d60e9d6fe02d047c528c845033c0a
Pratyush Mahapatra <pratyush@cs.wisc.edu>
2019-03-08 01:19:55 -0600

    added ngram-5.pkl

--- Source/test.py
+++ Source/test.py
@@ -20,4 +20,22 @@ data3 = pd.read_pickle(f)
 f.close()
 data3["word"] = data3["w0"] + " " + data3["w1"] + " " + data3["w2"]
 pd.concat([data3, data3["word"]], axis=1, ignore_index=True)
-data3.drop(["w0", "w1", "w2"], axis=1, inplace=True) 
+data3.drop(["w0", "w1", "w2"], axis=1, inplace=True)
+
+f = open('../data/data_window-4_ngram-5.pkl', 'rb')
+data4 = pd.read_pickle(f)
+f.close()
+data4["word"] = data4["w0"] + " " + data4["w1"] + " " + data4["w2"] + " " + data4["w3"]
+pd.concat([data4, data4["word"]], axis=1, ignore_index=True)
+data4.drop(["w0", "w1", "w2", "w3"], axis=1, inplace=True)
+
+data = pd.concat([data1,data2,data3,data4], ignore_index=True)
+
+f = open('../data/data_window_ngram-5.pkl', 'wb')
+data.to_pickle(f)
+f.close()
+
+f = open('../data/data_window_ngram-5.pkl', 'rb')
+some = pd.read_pickle(f)
+print(some)
+f.close()
