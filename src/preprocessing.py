49559abcd35c60570c97b8a69b4ed397b545256e
Pratyush Mahapatra <pratyush@cs.wisc.edu>
2019-03-08 20:03:25 -0600

    Create preprocessing.py

---
+++ src/preprocessing.py
@@ -0,0 +1,17 @@
+import pandas as pd
+#import pickle
+import sys
+from tqdm import tqdm
+sys.path.append('../utils')
+import letterFeatures
+
+def preprocessing(data):
+	for index, row in tqdm(data.iterrows()):
+		if (letterFeatures.allLower(row) == 1):
+			data.drop([index], inplace=True)
+	return data
+
+#f = open('../data/data_window_ngram-5.pkl', 'rb')
+#data = pd.read_pickle(f)
+#f.close()
+#print(preprocessing(data[:1000]))
