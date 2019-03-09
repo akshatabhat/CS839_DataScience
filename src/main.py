fd7515bf8090b35328e36c60d30be6432543e551
Akshata <mail.akshata18@gmail.com>
2019-03-09 01:26:04 -0600

    Save pre-processing data to pkl

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import ner_model
 import pandas as pd
 import pickle
 import preprocessing
+import argparse
 
 def load_data(input_filename):
 	print("---------- Loading the data ---------- ")
@@ -14,13 +15,15 @@ def load_data(input_filename):
 	
 
 if __name__ == '__main__':
-	
+
 	# Load and read data into pandas dataframe
 	input_filename = '../data/data_window_ngram-5.pkl'
 	data = load_data(input_filename)
 
 	# Preprocessing
 	processed_data = preprocessing.preprocessing(data)
+
+    data.to_pickle("../data/data_window_ngram-5-processed.pkl")
 
 	# Build Named Entity Recognizer.
 	ner_model.build_ner_model(processed_data)
