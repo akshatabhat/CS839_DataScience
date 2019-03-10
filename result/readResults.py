0e8b9e9f8b29d28fefb5075ab7e5ac66922364dc
Pratyush Mahapatra <pratyush@cs.wisc.edu>
2019-03-10 16:46:18 -0500

    more changes

--- result/readResults.py
+++ result/readResults.py
@@ -3,11 +3,11 @@ import pandas as pd
 f = open("Random Forest_false_pos.pkl",'rb')
 data = pd.read_pickle(f)
 for index, row in data.iterrows():
-	print("%s %s" %(row["file_ids"], row["word"]))
+	print("%4s | %30s | %30s | %30s" %(row["file_ids"], row["prev4"], row["word"], row["after0"]))
 f.close()
 
 f = open("Random Forest_false_pos_train.pkl",'rb')
 data = pd.read_pickle(f)
 for index, row in data.iterrows():
-	print("%s %s" %(row["file_ids"], row["word"]))
+	print("%4s | %30s | %30s | %30s" %(row["file_ids"], row["prev4"], row["word"], row["after0"]))
 f.close()
