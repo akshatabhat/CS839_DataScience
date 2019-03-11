59a03d550f9833db67e54f6b6743677f67eb3f8a
Pratyush Mahapatra <pratyush@cs.wisc.edu>
2019-03-10 19:13:13 -0500

    Update dictFeatures.py

--- utils/dictFeatures.py
+++ utils/dictFeatures.py
@@ -18,4 +18,19 @@ def dictionaryTwoLetterCapitalWordexceptUSUKEU(data):
 				return 0
 			else :
 				return 1
+	elif (word[0:2].isupper() == True) :
+		if (word[3].isalpha() == False) :
+			if (word[0:2] == "US") :
+				return 0
+			elif (word[0:2] == "UK") :
+				return 0
+			elif (word[0:2] == "EU") :
+				return 0
+			elif (word[0:2] == "NZ") :
+				return 0
+			elif (word[0:2] == "SA") :
+				return 0
+			else :
+				return 1
+		return 0
 	return 0	
