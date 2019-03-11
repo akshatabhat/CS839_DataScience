e7201ea70b44fc7832cc570e7375ea33493b3e68
Akshata <mail.akshata18@gmail.com>
2019-03-10 23:39:13 -0500

    Moving Dataset processing files

--- utils/CompileDataset.py
+++ utils/CompileDataset.py
@@ -15,12 +15,15 @@ window_sizes = [1,2]
 
 for window_size in window_sizes:
     #### Paths and filenames
-    in_path = "./FileRepo_Annotated/"
-    out_path = "./Data/"
+    in_path = "../FileRepo_Annotated/"
+    out_path = "../Data/"
     out_filename = "data"
     #### Files to parse. 
     ## Files that are currently annotated: 1-110, 200-310, 400-510.
-    file_ids = [485]
+    # file_ids = list(range(0,110))
+    file_ids = list(range(0,110))+list(range(200,310))+list(range(400,510))
+    # file_ids = ["101","103","201","222","228","401","402","459"]
+    # file_ids = ["101","103","201","432","276"]
     #### Parsing parameters
     ## Number of words that we look at one time.
     # window_size = 1
@@ -41,14 +44,7 @@ for window_size in window_sizes:
         ## Append to dataframe
         df = pd.concat([df,curr_df],axis=0)
 
+    ## Save data
     out_filename_info = "_window-" + str(window_size) + "_ngram-" + str(ngram)
-
-    ## Load previous dataframe and remove all entries
-    df_prev = pd.read_pickle(out_path + out_filename + out_filename_info + ".pkl")
-    for ID in file_ids:
-        df_prev = df_prev[df_prev.file_ids != ID]
-    ## Add entries for dataframes that were redone
-    df_prev = pd.concat([df_prev,df],axis=0)
-
-    df_prev.to_pickle(out_path + out_filename + out_filename_info + ".pkl")
+    df.to_pickle(out_path + out_filename + out_filename_info + ".pkl")
 
