92c332a6eb51cee37af2546d77ac703d6a850ada
Pratyush Mahapatra <pratyush@cs.wisc.edu>
2019-04-06 01:16:17 -0500

    Update testExtractor.py

--- stage2/testExtractor.py
+++ stage2/testExtractor.py
@@ -1,10 +1,16 @@
 from lxml import html
 import requests
 
-page = requests.get('https://arxiv.org/search/?query=data+science&searchtype=all&source=header')
+page = requests.get('https://arxiv.org/search/advanced?advanced=&terms-0-operator=AND&terms-0-term=cs.cv&terms-0-field=all&classification-physics_archives=all&classification-include_cross_list=include&date-year=&date-filter_by=date_range&date-from_date=2011&date-to_date=2018&date-date_type=submitted_date&abstracts=show&size=50&order=-announced_date_first')
 tree = html.fromstring(page.content)
 
-title = tree.xpath('//p[@class="title is-5 mathjax"]/text()')
-authors = tree.xpath('//p[@class="authors"]/text()')
-submitted = tree.xpath('//p[@class="is-size-7"]/text()')
-print(submitted)
+title = tree.xpath('/html/body/main/div[2]/ol/li/p[@class="title is-5 mathjax"]/text()')
+authors = tree.xpath('/html/body/main/div[2]/ol/li/p[@class="authors"]/text()')
+submitted = tree.xpath('/html/body/main/div[2]/ol/li/p[@class="is-size-7"]/text()')
+#print(title)
+nextpage = tree.xpath('/html/body/main/div/nav/a[@class="pagination-next"]/@href')
+page = requests.get("http:/" + nextpage[0])
+title += tree.xpath('/html/body/main/div[2]/ol/li/p[@class="title is-5 mathjax"]/text()')
+authors += tree.xpath('/html/body/main/div[2]/ol/li/p[@class="authors"]/text()')
+submitted += tree.xpath('/html/body/main/div[2]/ol/li/p[@class="is-size-7"]/text()')
+print(title)
