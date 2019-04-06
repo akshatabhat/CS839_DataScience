249952dfb912bd12a8ec6a8b37192c4ec7fb4861
Pratyush Mahapatra <pratyush@cs.wisc.edu>
2019-04-05 23:50:17 -0500

    Update testExtractor.py

--- stage2/testExtractor.py
+++ stage2/testExtractor.py
@@ -4,4 +4,5 @@ import requests
 page = requests.get('https://arxiv.org/search/?query=data+science&searchtype=all&source=header')
 tree = html.fromstring(page.content)
 
-print(tree)
+authors = tree.xpath('//p[@class="title is-5 mathjax"]/text()')
+print(authors)
