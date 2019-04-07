ac501352372cb7951437eaf6a6426b034cb4b008
Akshata <mail.akshata18@gmail.com>
2019-04-07 18:20:45 -0500

    web scrapping arxiv

--- stage2/testExtractor.py
+++ stage2/testExtractor.py
@@ -1,18 +1,25 @@
 from lxml import html
 import requests
-
-page = requests.get('https://arxiv.org/search/advanced?advanced=&terms-0-operator=AND&terms-0-term=cs.cv&terms-0-field=all&classification-physics_archives=all&classification-include_cross_list=include&date-year=&date-filter_by=date_range&date-from_date=2011&date-to_date=2018&date-date_type=submitted_date&abstracts=show&size=50&order=-announced_date_first')
+import pandas as pd
+import numpy as np
+page = requests.get('https://arxiv.org/search/advanced?advanced=&terms-0-term=cs.cv&terms-0-operator=AND&terms-0-field=all&classification-physics_archives=all&classification-include_cross_list=include&date-filter_by=date_range&date-year=&date-from_date=2011&date-to_date=2018&date-date_type=submitted_date&abstracts=show&size=200&order=-announced_date_first')
 tree = html.fromstring(page.content)
 
-title = tree.xpath('/html/body/main/div[2]/ol/li/p[@class="title is-5 mathjax"]/text()')
-authors = tree.xpath('/html/body/main/div[2]/ol/li/p[@class="authors"]/text()')
-submitted = tree.xpath('/html/body/main/div[2]/ol/li/p[@class="is-size-7"]/text()')
-#print(title)
-nextpage = tree.xpath('/html/body/main/div/nav/a[@class="pagination-next"]/@href')
-#print(nextpage[0])
-page = requests.get("https://arxiv.org" + nextpage[0])
-tree = html.fromstring(page.content)
-title += tree.xpath('/html/body/main/div[2]/ol/li/p[@class="title is-5 mathjax"]/text()')
-authors += tree.xpath('/html/body/main/div[2]/ol/li/p[@class="authors"]/text()')
-submitted += tree.xpath('/html/body/main/div[2]/ol/li/p[@class="is-size-7"]/text()')
-print(title)
+results_per_page = 200
+total_pages = 50 #int(tree.xpath('/html/body/main/div[1]/div[1]/h1/text()')[0].replace('\n','').strip().split('of')[1].strip().split(' ')[0].replace(',',''))//results_per_page
+df = pd.DataFrame(columns=['Tag','Title', 'Authors','Month', 'Year'])
+for i in range(0, total_pages):
+	for j in range(1, results_per_page+1):
+		tag = tree.xpath('/html/body/main/div[2]/ol/li['+str(j)+']/div/p/a/text()')[0]
+		title = tree.xpath('/html/body/main/div[2]/ol/li['+str(j)+']/p[@class="title is-5 mathjax"]/text()')[0].replace('\n','').strip()
+		authors = ', '.join(tree.xpath('/html/body/main/div[2]/ol/li['+str(j)+']/p[@class="authors"]/a/text()'))
+		submitted = tree.xpath('/html/body/main/div[2]/ol/li['+str(j)+']/p[@class="is-size-7"]/text()')[0].replace(';','').replace('\n','').strip().split(',')
+		month = submitted[0].split(' ')[1]
+		year = submitted[1]
+		df.loc[i*results_per_page+j] = [tag, title, authors, month, year]
+	nextpage = tree.xpath('/html/body/main/div/nav/a[@class="pagination-next"]/@href')
+	page = requests.get("https://arxiv.org" + nextpage[0])
+	tree = html.fromstring(page.content)	
+#print(df)
+
+df.to_csv('./data/arxiv.csv', index=False)
