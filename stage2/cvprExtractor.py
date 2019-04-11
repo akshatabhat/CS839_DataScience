43155005ae7046435693c9dcf06b9c5e7bfeb5f4
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2019-04-10 21:58:00 -0500

    add new cols to table

--- stage2/cvprExtractor.py
+++ stage2/cvprExtractor.py
@@ -2,6 +2,7 @@ from lxml import html
 import requests
 import pandas as pd
 import numpy as np
+import time
 from IPython.core import debugger
 breakpoint = debugger.set_trace
 
@@ -13,10 +14,11 @@ breakpoint = debugger.set_trace
 # 5. Year
 
 # Note: For some reason the 2013 cvpr page contains ICCV 2013 workshop papers instead of cvpr so we dont add it here. 
-cvpr_years = ['2014', '2015', '2016', '2017', '2018']
+cvpr_years = ['2014','2015', '2016', '2017', '2018']
+# cvpr_years = ['2018']
 
 webpage_prefix = 'http://openaccess.thecvf.com/'
-df = pd.DataFrame(columns=['Tag','Title', 'Authors','Month', 'Year'])
+df = pd.DataFrame(columns=['Tag','Title', 'Authors','Month', 'Year', 'JournalRef', 'Abstract'])
 total_paper_count = 0
 
 
@@ -28,7 +30,6 @@ for year in cvpr_years:
 
     papers_data = tree.xpath('//div[@class="bibref"]/text()')
     n_papers = int(len(papers_data) / 7)
-
     for i in range(n_papers):
         st_idx = i*7
         end_idx = st_idx + 7
@@ -50,12 +51,31 @@ for year in cvpr_years:
         year_data = curr_paper_data[5]
         year = year_data[9:-1]
 
+        ## Get current paper link to page
+        paper_href = tree.xpath('//*[@id="content"]/dl/dt[{}]/a/@href'.format(i+1))
+        paper_link = webpage_prefix + paper_href[0]
+        journal = ''
+        abstract = ''
+        ## Extract journal ref and abstract from paper's webpage
+        curr_page = requests.get(paper_link)
+        curr_tree = html.fromstring(curr_page.content)
+        ## a few papers have a broken link to their webpage
+        if(curr_page.ok):
+            # Journal        
+            journalref_data = curr_tree.xpath('//*[@id="authors"]/text()')[1]
+            journalref = journalref_data[2:-1] ## skip empty and newline characters
+            # Abstract        
+            abstract_data = curr_tree.xpath('//*[@id="abstract"]/text()')[0]
+            abstract = abstract_data[1:] # skip first character which is \n
+        else:
+            breakpoint()
         ## Add row to data framce
-        row_data = [tag,title,authors,month,year]
+        row_data = [tag,title,authors,month,year, journalref, abstract]
         df.loc[total_paper_count + i] = row_data
 
-        print('Tag: {}\n Title: {}\n Authors: {}\n Month: {}\n Year: {}\n'.format(tag,title,authors,month,year))
-
+        if( i % 25 == 0): 
+            print('Tag: {}\n Title: {}\n Authors: {}\n Month: {}\n Year: {}\n Journalref: {}\n Abstract: {}\n'.format(tag,title,authors,month,year, journalref, abstract[0:10]))
+            time.sleep(2)
     total_paper_count = total_paper_count + n_papers
     
 
